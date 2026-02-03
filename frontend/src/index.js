5f84abf1990a68bdf06379166248aa665b8bfcca
Jadeja Dhruvrajsinh Rajendrasinh <23it035@charusat.edu.in>
2026-02-03 18:39:21 +0530

    Fix development Issue

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import './index.css';
 import App from './App';
+import API_URL from './config/apiConfig';
+
+// Configure axios base URL
+axios.defaults.baseURL = API_URL;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
